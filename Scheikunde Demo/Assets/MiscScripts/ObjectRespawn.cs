a140ce0ee7e5e01d154a60927462f7632dacabc3
Remasu <stefanherder2001@gmail.com>
2021-09-27 12:44:37 +0200

    Grabbable respawn & some random files

---
+++ Scheikunde Demo/Assets/MiscScripts/ObjectRespawn.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRespawn : MonoBehaviour
+{
+    public Vector3 position;
+    public Quaternion rotation;
+    public float timeBeforeRespawn = 10.0f;
+    public float t;
+
+    private void Awake()
+    {
+        position = transform.position;
+        rotation = transform.rotation;
+    }
+
+    private void Update()
+    {
+        RespawnCheckLoop();
+    }
+
+    public void RespawnCheckLoop()
+    {
+        if (transform.position.y <= 0.4f) //around leg height
+        {
+            t += Time.deltaTime;
+        }
+        else
+        {
+            t = 0;
+        }
+
+        if (t >= timeBeforeRespawn)
+        {
+            transform.position = position;
+            transform.rotation = rotation;
+            t = 0;
+
+            if (GetComponent<Rigidbody>())
+            {
+                var r = GetComponent<Rigidbody>();
+
+                r.velocity = Vector3.zero;
+                r.angularVelocity = Vector3.zero;
+            }
+        }
+    }
+}
